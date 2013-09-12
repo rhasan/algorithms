fd4fe3ccdd29127d57d57ec4f9fc05992739b96a
Rakebul Hasan <rakeb04@gmail.com>
2013-09-12 19:49:19 +0200

    uniform cost search partiel implementation added

--- graph.py
+++ graph.py
@@ -1,13 +1,109 @@
-
+from collections import defaultdict
+"""Class for a graph node"""
 class Node:
-    """Class for a graph node"""
+    """Initializes a node with a node id and label"""
     def __init__(self,node_id,label):
         self.node_id = node_id
         self.label = label
+    
+    def __key(self):
+        return self.node_id
 
-class Edge(object):
-    """Class for a graph edge"""
-    def __init__(self, node_u,node_v,label):
-        self.node_u = node_u
-        self.node_v = node_v
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __repr__(self):
+        return self.label
+
+"""Class for a graph edge"""
+class Edge:
+    """
+    Initializes an edge with a start node and end node and a label
+    start and end are instances of Node class.
+    """
+    def __init__(self, start, end,label):
+        self.start = start
+        self.end = end
         self.label = label
+
+    def __key(self):
+        #return (self.start,self.label,self.end) #for multi edge
+        return (self.start,self.end) #for single edge per node pair
+
+    def __hash__(self):
+        return hash(self.__key())
+
+    def __repr__(self):
+        return str((self.start.label,self.label,self.end.label))
+
+
+"""Class for Graphs"""
+class Graph(object):
+    
+    """
+    Initializes the graph.
+    """
+    def __init__(self):
+        self.edges = defaultdict(lambda: list())
+        self.node_dict = dict()
+        #self.nodes_list = list()
+
+    """
+    Adds a Node to the graph.
+    """
+    def add_node(self,node):
+        self.node_dict[node.node_id] = node
+
+    """
+    Returns the list of Node instances in the graph.
+    """
+    def node_list(self):
+        return self.node_dict.values()
+
+    """
+    Adds an Edge to the graph.
+    """
+    def add_edge(self,edge):
+        self.edges[edge.start.node_id].append(edge)
+
+    """
+    Returns the Node for the node_id.
+    """
+    def node(self,node_id):
+        return self.node_dict[node_id]
+
+    """
+    Returns the list of outgoing edges from node.
+    """
+    def outgoing_edges(self,node):
+        return self.edges[node.node_id]
+
+    """
+    Returns the list adjacent nodes
+    """
+    def adjacent(self,node):
+        adj = list()
+        edgs = self.edges[node.node_id]
+        for edg in edgs:
+            adj.append(edg.end)
+        return adj
+
+    """
+    Returns the number of nodes in the graph.
+    """
+    def size(self):
+        return len(self.node_dict)
+
+    """
+    Print the graph.
+    """
+    def pretty_print(self):
+        nodes = self.node_list()
+
+        for node in nodes:
+            if len(self.edges[node.node_id])>0:
+                print "Outgoing edges from node", node, ":"
+            for edge in self.edges[node.node_id]:
+                print edge
+
+        
---
+++ priority_queue.py
@@ -0,0 +1,69 @@
+from heapq import *
+import itertools
+
+class CustomPriorityQueue:
+
+    def __init__(self):
+        self.pq = []                         # list of entries arranged in a heap
+        self.entry_finder = {}               # mapping of tasks to entries
+        self.REMOVED = '<removed-task>'      # placeholder for a removed task
+        #self.counter = itertools.count()     # unique sequence count
+        self.N = 0
+
+
+    def add(self,entry):
+        'Add a new entry or update the priority of an existing task'
+        (pr,val) = entry
+        if val in self.entry_finder:
+            remove_task(val)
+        entry_list = [pr,val]
+        self.entry_finder[val] = entry_list
+        heappush(self.pq, entry_list)
+
+        self.N += 1
+
+    def remove(self,task):
+        'Mark an existing task as REMOVED.  Raise KeyError if not found.'
+        entry = self.entry_finder.pop(task)
+        entry[-1] = self.REMOVED
+        self.N -= 1
+
+    def pop(self):
+        'Remove and return the lowest priority task. Raise KeyError if empty.'
+        while self.pq:
+            priority, task = heappop(self.pq)
+            if task is not self.REMOVED:
+                del self.entry_finder[task]
+                self.N -= 1
+                return (priority, task)
+        raise KeyError('pop from an empty priority queue')
+    
+    def empty(self):
+        return self.N == 0
+
+    def replace(self,find,replace):
+        
+        self.remove(find)
+        self.add(replace)
+        
+
+
+def main():
+    pq = CustomPriorityQueue()
+
+    data = [(10,"ten"), (3,"three"), (5,"five"), (7,"seven"), (9, "nine"), (2,"two")]
+
+    for item in data:
+        print item
+        pq.add(item)
+
+    pq.replace("ten",(1,"one"))
+    #print pq.counter
+    sorted_list = []
+
+    while pq.empty()==False:
+        sorted_list.append(pq.pop())
+    print sorted_list
+
+if __name__ == "__main__":
+    main()
